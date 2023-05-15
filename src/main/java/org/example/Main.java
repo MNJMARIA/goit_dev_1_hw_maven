5ba727ca780a4de1a7a7f17e41c6d8ff0366f9b7
Maria <stepanovamasha70@gmail.com>
2023-05-15 22:17:03 +0300

    add gson dependency

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import com.google.gson.Gson;
+
+public class Main {
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+        Person person = new Person("Maria", "Stepanova");
+
+        System.out.println(gson.toJson(person));
+    }
+}
